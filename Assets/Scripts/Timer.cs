ae76e86d5d229bc2580041d8dee5d624f350b558
OwnedCoot <aobriencollege@gmail.com>
2024-10-09 15:16:45 -0400

    Updated timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    private float time;
+    private float curTime;
     private TMP_Text guiTime;
     // Start is called before the first frame update
     void Start()
